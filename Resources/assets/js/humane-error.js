12f06b40a357c8eacd2f5b874dda0993a844b95e
Thomas Beaujean <t.beaujean@a5sys.com>
2015-11-16 14:46:27 +0100

    Checkstyle

--- Resources/assets/js/humane-error.js
+++ Resources/assets/js/humane-error.js
@@ -1,2 +1,4 @@
 
-humane.error = humane.spawn({ addnCls: 'humane-libnotify-error', timeout: 5000 })
+/* global humane */
+
+humane.error = humane.spawn({ addnCls: 'humane-libnotify-error', timeout: 5000 });
